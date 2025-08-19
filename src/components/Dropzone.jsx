1eddd8440cf87293092d86882b7fbedb6b2df06e
realizer <ipreet35232@gmail.com>
2025-08-19 16:15:43 +0530

    changed dropzone animation and added uploadForm

--- src/components/Dropzone.jsx
+++ src/components/Dropzone.jsx
@@ -1,13 +1,55 @@
 import { Button } from "../components";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
-const Dropzone = ({ onClick }) => {
+const UploadForm = ({ video }) => {
+    const [thumbnail, setThumbnail] = useState(null);
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const formData = new FormData();
+        formData.append("title", title);
+        formData.append("description", description);
+        formData.append("video", video);
+        formData.append("thumbnail", thumbnail);
+        console.log(formData);
+    }
+    return (
+        <>
+            <form className="grid mt-10 mx-10 gap-10 place-items-center" onSubmit={handleSubmit}>
+                <input type="text" name="" value={title} placeholder="title (required)"
+                    className="w-full border rounded text-sm py-1 px-2"
+                    onChange={(e) => setTitle(e.target.value)} />
+                <textarea rows="" cols="" value={description} placeholder="description"
+                    className="w-full border rounded text-sm py-1 px-2"
+                    onChange={(e) => setDescription(e.target.value)}
+                ></textarea>
+                <div className="w-full space-x-4">
+                    <label htmlFor="thumbnail" className="cursor-pointer text-sm">Select Thumbnail</label>
+                    <input id="thumbnail" type="file" name="" accept=".png,.jpg,.jpeg,.webp,.heic"
+                        className="border rounded text-sm py-1 px-2 cursor-pointer"
+                        onChange={(e) => setThumbnail(e.target.files[0])} />
+                </div>
+                <Button type="submit">upload</Button>
+            </form>
+        </>
+    )
+};
+
+const Dropzone = () => {
+    const [video, setVideo] = useState(null);
     const [isDraging, setIsDraging] = useState(false);
-    const [file, setFile] = useState(null);
     const [status, setStatus] = useState("idle");
-    const [uploadProgress, setUploadProgress] = useState(0);
     const dragCounter = useRef(0);
     const inputRef = useRef(null);
+
+    useEffect(() => {
+        if (video && status !== "droped") {
+            setIsDraging(true);
+            const timer = setTimeout(() => setIsDraging(false), 300);
+            return () => clearTimeout(timer); // clean-up
+        }
+    }, [video]);
 
     const handleDragOver = (e) => { e.preventDefault(); };
     const handleDragEnter = (e) => {
@@ -26,25 +68,22 @@ const Dropzone = ({ onClick }) => {
         e.preventDefault();
         dragCounter.current = 0;
         setIsDraging(false);
-        setFile(e.dataTransfer.files[0]);
-        setStatus("droped");
+        const file = e.dataTransfer.files[0];
+        if (file.type.startsWith("video/")) {
+            setVideo(file);
+            setStatus("droped");
+        } else {
+            setStatus("invalid");
+            console.log("invalid file");
+        }
     };
 
     const handleClick = () => { inputRef.current.click(); };
     const handleFileChange = (e) => {
-        if (e.target.files) setFile(e.target.files[0])
+        if (e.target.files) setVideo(e.target.files[0])
     }
-    const handleFileUpload = async () => {
-        if (!file) return;
-        setStatus("uploading");
-        const formData = new FormData();
-        formData.append("file", file);
-        try {
-            await fetch();
-        } catch (error) {
 
-        }
-    }
+    if (status === "uploaded") return <UploadForm video={video} />
 
     return (
         <>
@@ -54,20 +93,24 @@ const Dropzone = ({ onClick }) => {
                     <div className="size-30 rounded-full bg-gray-900 text-white p-8 inline-flex items-end justify-center
                     cursor-pointer relative overflow-hidden" onClick={handleClick}>
                         <div className="grid place-items-center relative z-10">
-                            <div className={`${status === "droped" ? "animate-takeoff" : ""} grid place-items-center`}>
-                                <div className="w-0 h-0 border-l-[20px] border-l-transparent border-r-[20px]
-                        border-r-transparent border-b-[20px] border-b-gray-400"></div>
-                                <div className={`bg-gray-400 ${isDraging ? "w-5 h-2" : "w-3 h-5"} duration-200`} ></div>
+                            <div className={`grid place-items-center duration-200
+                                    ${isDraging ? "translate-y-2" : !isDraging && video ? "animate-takeoff" : ""}`}
+                                onAnimationEnd={() => setStatus("uploaded")}>
+                                <div className={`w-0 h-0 border-l-20 border-l-transparent border-r-20
+                                    border-r-transparent border-b-20 border-b-gray-400 duration-200
+                                    ${isDraging ? "translate-y-2" : ""}`}></div>
+                                <div className={`bg-gray-400 duration-200 w-4 h-4
+                                    ${isDraging ? "scale-x-150 scale-y-80" : ""}`} ></div>
                             </div>
-                            <div className={`bg-gray-400 w-10 h-2 mt-1 ${status === "droped" ? "opacity-0" : "opacity-100"} duration-200`}></div>
+                            <div className={`bg-gray-400 w-10 h-2 mt-2 ${video ? "opacity-0" : "opacity-100"} duration-500`}></div>
                         </div>
                         <div className={`w-30 h-150 absolute bg-[url(/src/assets/speed_line_darkmode.svg)]
-                         -translate-y-30 ${status === "droped" ? "animate-speedline" : ""}`}></div>
+                         -translate-y-30 ${video ? "animate-speedline" : ""}`}></div>
                     </div>
                     <p>Drag and drop video files to upload</p>
                     <Button onClick={handleClick}>Select Video</Button>
                 </div>
-            </div>
+            </div >
             <input type="file" name="" accept=".mp4,.mkv,.webm" className="hidden" ref={inputRef}
                 onChange={handleFileChange} />
         </>
