f4a49fd438a19d6cf2c6a4f26883c81f5ebfa5ec
realizer <ipreet35232@gmail.com>
2025-09-08 17:33:47 +0530

    volume button styles and accessblity

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,10 +1,11 @@
-import { Maximize, Minimize, Pause, Play, RotateCcw, Volume2, VolumeOff } from "lucide-react";
+import { Captions, ChevronRight, CircleGauge, Maximize, Minimize, Pause, Play, RotateCcw, Settings, Settings2, Volume2, VolumeOff } from "lucide-react";
 import { useEffect, useRef, useState } from "react"
 
 export default function VideoPlayer({ videoLink }) {
     const videoRef = useRef(null);
     const progressRef = useRef(null);
     const containerRef = useRef(null);
+    const volumeDivRef = useRef(null);
 
     const [mute, setMute] = useState(false);
     const [play, setPlay] = useState(false);
@@ -14,12 +15,15 @@ export default function VideoPlayer({ videoLink }) {
     const [fullScreen, setFullScreen] = useState(false);
     const [volume, setVolume] = useState(1);
     const [ended, setEnded] = useState(false);
+    const [showSettings, setShowSettings] = useState(false);
+    const [playbackRate, setPlaybackRate] = useState(1);
 
     const progressPercent = duration > 0 ? (currentTime / duration) * 100 : 0;
 
     useEffect(() => {
         const video = videoRef.current;
         const container = containerRef.current;
+        const volumeDiv = volumeDivRef.current;
 
         const updateTime = () => {
             setCurrentTime(video.currentTime);
@@ -52,6 +56,7 @@ export default function VideoPlayer({ videoLink }) {
             video.addEventListener("ended", handleEnd);
             video.addEventListener("play", handlePlay);
             container.addEventListener("fullscreenchange", handleFullscreenChange);
+            volumeDiv.addEventListener("wheel", handleWheel, { passive: false });
         }
 
         return () => {
@@ -119,10 +124,10 @@ export default function VideoPlayer({ videoLink }) {
             case "j":
                 skip(-10);
                 break;
-            case "<":
+            case ">":
                 changePlaybackRate(+0.25);
                 break;
-            case ">":
+            case "<":
                 changePlaybackRate(-0.25);
                 break;
         }
@@ -144,23 +149,32 @@ export default function VideoPlayer({ videoLink }) {
         videoRef.current.currentTime = newTime;
         setCurrentTime(newTime);
     };
-    const handleVolumeChange = (e) => {
-        const newVolume = parseFloat(e.target.value);
-        setVolume(newVolume);
-        videoRef.current.volume = newVolume;
-        setMute(newVolume === 0);
+    const handleVolumeChange = (newVolume) => {
+        const clamped = Math.max(0, Math.min(1, parseFloat(newVolume).toFixed(2)));
+        setVolume(clamped);
+        videoRef.current.volume = clamped;
+        setMute(clamped === 0);
     };
     const skip = (seconds) => {
         const video = videoRef.current;
         video.currentTime = Math.max(0, Math.min(duration, video.currentTime + seconds));
     };
     const changePlaybackRate = (rate) => {
+        console.log(rate, typeof rate);
         const video = videoRef.current;
         const currentRate = video.playbackRate;
         const limit = currentRate + rate <= 2 && currentRate + rate >= 0.25;
         if (limit) {
             video.playbackRate = currentRate + rate;
         }
+    };
+    const handleWheel = (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+
+        const delta = e.deltaY > 0 ? -0.05 : 0.05;
+        let newVolume = videoRef.current.volume + delta;
+        handleVolumeChange(newVolume);
     };
 
     return (
@@ -171,15 +185,16 @@ export default function VideoPlayer({ videoLink }) {
                 Your browser does not support the video tag.
             </video>
             <div className="px-4 absolute bottom-0 inset-x-0">
-                <div className="relative flex w-full bg-papaya-whip/50 cursor-pointer h-1 hover:h-1.5 duration-200"
-                    ref={progressRef} onClick={handleProgressClick}>
-                    <div className="flex items-center relative z-10 bg-fire-brick h-full"
-                        style={{ width: `${progressPercent}%` }}>
-                        <div className="absolute size-4 rounded-full bg-fire-brick -right-0.5"></div>
-                    </div>
-                    <div className="absolute inset-0  w-full overflow-hidden">
-                        <div className="absolute h-full bg-papaya-whip/50"
-                            style={{ width: `${bufferPercent}%`, left: `${progressPercent}%` }}></div>
+                <div className="group nise pt-2 cursor-pointer" ref={progressRef} onClick={handleProgressClick}>
+                    <div className="relative flex w-full bg-papaya-whip/50  h-1 group-hover:h-1.5 duration-200">
+                        <div className="flex items-center relative z-10 bg-fire-brick h-full"
+                            style={{ width: `${progressPercent}%` }}>
+                            <div className="absolute size-4 rounded-full bg-fire-brick -right-0.5"></div>
+                        </div>
+                        <div className="absolute inset-0  w-full overflow-hidden">
+                            <div className="absolute h-full bg-papaya-whip/50"
+                                style={{ width: `${bufferPercent}%`, left: `${progressPercent}%` }}></div>
+                        </div>
                     </div>
                 </div>
                 <div className="flex items-center text-white my-2 space-x-4">
@@ -192,24 +207,50 @@ export default function VideoPlayer({ videoLink }) {
                             <Play />
                         )}
                     </button>
-                    <div className="flex items-center space-x-1">
+                    <div className="group flex items-center space-x-1" ref={volumeDivRef}>
                         <button className="cursor-pointer" onClick={toggleMute}>
                             {mute ? <VolumeOff /> : <Volume2 />}
                         </button>
                         <input type="range" min={0} max={1} step={0.01} value={mute ? 0 : volume}
-                            onChange={handleVolumeChange} className="w-20 h-1 rounded-lg bg-black" />
+                            onChange={(e) => handleVolumeChange(e.target.value)} className="w-0 h-1 rounded-lg appearance-none
+                            volume-slider group-hover:w-15 group-hover:opacity-100 opacity-0 duration-200"
+                            style={{
+                                background: `linear-gradient(to right, hsla(40, 91%, 91%, 1) 0%, hsla(40, 91%, 91%, 1)
+                                ${(mute ? 0 : volume) * 100}%, hsla(40, 91%, 91%, 0.2) ${(mute ? 0 : volume) * 100}%,
+                                hsla(40, 91%, 91%, 0.2) 100%`}} />
                     </div>
                     <span className="text-sm">
                         {formatTime(currentTime) + " / " + formatTime(duration)}
                     </span>
-                    <div className="ml-auto">
+                    <div className="flex ml-auto space-x-4">
+                        <button className={`cursor-pointer ${showSettings ? "rotate-30" : ""} duration-200`}
+                            onClick={() => setShowSettings(!showSettings)}>
+                            <Settings />
+                        </button>
                         <button className="cursor-pointer" onClick={toggleFullscreen}>
                             {fullScreen ? <Minimize /> : <Maximize />}
                         </button>
                     </div>
                 </div>
             </div>
-        </div>
+            {
+                showSettings &&
+                <ul className="py-2 absolute right-2 bottom-13 bg-black/90 text-papaya-whip rounded-md text-nowrap">
+                    <li className="flex items-center gap-2 hover:bg-gray-600 cursor-pointer py-2 px-4 duration-200">
+                        <CircleGauge /> Playback Speed <span>{playbackRate === 1 ? "normal" : playbackRate}</span>
+                        <ChevronRight />
+                    </li>
+                    <li className="flex itmes-center gap-2 hover:bg-gray-600 cursor-pointer py-2 px-4 duration-200">
+                        <Captions /> Subtitles/CC <span>none</span>
+                        <ChevronRight />
+                    </li>
+                    <li className="flex items-center gap-2 hover:bg-gray-600 cursor-pointer py-2 px-4 duration-200">
+                        <Settings2 /> Quality <span>1080</span>
+                        <ChevronRight />
+                    </li>
+                </ul>
+            }
+        </div >
     )
 }
 
